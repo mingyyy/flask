5baaff7e3d4c0655cc9ed24fb4e4396767a55a14
Mingyyy <j.yanming@gmail.com>
2019-08-07 16:57:32 +0200

    Add chapter3. Adding forms, login and config.py file

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,5 +1,18 @@
 from app import app
-from flask import render_template
+from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        # get_flashed_messages() will render the messages stored in flash which will be deleted after
+        # the flash function is called.
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route('/index')
