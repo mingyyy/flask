1fd58140778700af675c23f900abbb520d570ffd
Mingyyy <j.yanming@gmail.com>
2019-08-07 11:15:46 +0200

    Complete Chapter2. Adding .flaskenv for export env variables and adding template

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,24 @@
 from app import app
+from flask import render_template
 
-@app.route('/')
+
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Mingy'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/')
+def main():
+    user = {'username': 'Ming'}
+    return render_template('base.html', user=user)
