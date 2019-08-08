10e9d523f69f46fffbfbbeaf41ed6581084138bb
Mingyyy <j.yanming@gmail.com>
2019-08-08 18:27:45 +0200

    Add last_seen and about_me for user in models.py

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -11,6 +11,8 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -33,7 +35,6 @@ class Post(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    author = db.Column(db.String(64), db.ForeignKey('user.username'))
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
