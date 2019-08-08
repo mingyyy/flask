82b2299accdf927c1bb9e1cd4fbd32a31a3a7937
Mingyyy <j.yanming@gmail.com>
2019-08-08 19:03:01 +0200

    Error fixing

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -74,7 +74,7 @@ def before_request():
         db.session.commit()
 
 
-app.route('/edit_profile', methods=['GET', 'POST'])
+@app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
     form = EditProfileForm()
