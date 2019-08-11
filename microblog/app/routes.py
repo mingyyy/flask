07a12dc6e5214d15ce570ca3d8dd8a98ff0ccc39
Mingyyy <j.yanming@gmail.com>
2019-08-11 23:29:54 +0200

    Display blog posts

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,5 +1,5 @@
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user
 from app.models import User, Post
@@ -23,13 +23,20 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    # user = {'username': 'Mingy'}
-    posts = Post.query.all()
-    return render_template("index.html", title='Home Page', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+
+    posts = current_user.followed_posts().all()
+    return render_template("index.html", title='Home Page', form=form, posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
