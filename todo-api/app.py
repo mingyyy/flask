f99f29d4a3ffe2ba2ce26d99396fdeca63c83c2d
Mingyyy <j.yanming@gmail.com>
2019-08-19 23:29:31 +0200

    Add second version

--- todo-api/app.py
+++ todo-api/app.py
@@ -15,7 +15,7 @@ def get_password(username):
 
 @auth.error_handler
 def unauthorized():
-    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
 
 
 tasks = [
